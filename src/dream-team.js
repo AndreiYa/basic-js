9fcea9d2ce9a4de71fc50370dfccae14714dc0fa
AndreiYa <newminer@bk.ru>
2020-09-28 23:55:08 +0300

    feat: add carbon, count, dream functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!Array.isArray(members)) {
+        return false;
+    }
+    
+    let twoLetterArr = [];
+
+    members.forEach(element => {
+        if (typeof (element) === 'string' && element != '') {
+            twoLetterArr.push(element.trim().toUpperCase().slice(0, 1));
+        } else {
+            return false;
+        }
+    });
+
+  return twoLetterArr.sort().join('');
+
 };
